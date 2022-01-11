ac4a25a9e4058ae6c8ef4c9d264d33e1b25f611f
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-01-10 22:05:19 -0500

    commit tree 94596a8c0cd4dd87c85dffd4734f71db918d0a5a

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+
+const random = 5
+
+const num3 = 4
+const num4 = 5
+const mod = num3 % num4
+
+const max = 20 
